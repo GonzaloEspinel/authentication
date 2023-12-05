1162a8db43a328f5f7a7718b41c59dc5c51bb583
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-12-04 20:15:36 -0500

    sync

---
+++ authentication/src/lib/require-auth.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { AuthContext } from ".auth-context";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+function RequireAuth({
+	children,
+	reload = false,
+	to = "/auth/login",
+}) {
+	const { currentUser } = useContext(AuthContext);
+	const location = useLocation();
+
+	if (!currentUser && reload) {
+		return children;
+	}
+
+	if (!currentUser) {
+		return (
+			<Navigate
+				to={`${to}?redirect=${location.pathname}`}
+				state={{ from: location }}
+				replace
+			/>
+		);
+	}
+
+	return children;
+}
+
+export default RequireAuth;
